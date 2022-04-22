9c6ae0389fbb6ea9dc516193f92e20d2a1ea2c4d
can sardaslar <101027564+sardaslar@users.noreply.github.com>
2022-04-21 23:58:13 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,24 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "375px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = 'blue';
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "125px";
+    document.getElementById("box").style.backgroundColor = 'orange' ;
+
+});
